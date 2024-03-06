f15e2dea8d5346b22f71bc638c34889c2f0215de
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2024-03-06 20:03:44 +0530

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,66 @@
 import { useState } from "react";
 import "./App.css";
 import axios from "axios";
+import ReactMarkdown from "react-markdown";
 
 function App() {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
+  const [generatingAnswer, setGeneratingAnswer] = useState(false);
 
-  async function generateAnswer() {
-    setAnswer("loading..");
-    const response = await axios({
-      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyBwRJ-crKN8cyEXD_6Tj36dF9dxJcclZRc",
-      method: "post",
-      data: {
-        contents: [{ parts: [{ text: question }] }],
-      },
-    });
+  async function generateAnswer(e) {
+    setGeneratingAnswer(true);
+    e.preventDefault();
+    setAnswer("Loading your answer... \n It might take upto 10 seconds");
+    try {
+      const response = await axios({
+        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyBwRJ-crKN8cyEXD_6Tj36dF9dxJcclZRc",
+        method: "post",
+        data: {
+          contents: [{ parts: [{ text: question }] }],
+        },
+      });
 
-    setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
+      setAnswer(
+        response["data"]["candidates"][0]["content"]["parts"][0]["text"]
+      );
+    } catch (error) {
+      setAnswer("Sorry - Something went wrong. Please try again!");
+    }
+
+    setGeneratingAnswer(false);
   }
 
   return (
     <>
-      <div className="w-full"></div>
-      <h1 className="bg-blue-300">Chat AI</h1>
-      <textarea
-        className="border rounded w-full"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-        cols="30"
-        rows="10"
-        placeholder="Ask anything to me"
-      ></textarea>
-      <button onClick={generateAnswer}>Generate answer</button>
-      <pre> {answer}</pre>
+      <div className="bg-white h-screen p-3">
+        <form
+          onSubmit={generateAnswer}
+          className="w-full md:w-2/3 m-auto text-center rounded bg-gray-50 py-2"
+        >
+          <a href="https://github.com/Vishesh-Pandey/chat-ai" target="_blank">
+            <h1 className="text-3xl text-center">Chat AI</h1>
+            <p className="text-xs">Using Google Gemini API</p>
+          </a>
+          <textarea
+            required
+            className="border rounded w-11/12 my-2 min-h-fit p-3"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            placeholder="Ask anything"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-blue-300 p-3 rounded-md hover:bg-blue-400 transition-all duration-300"
+            disabled={generatingAnswer}
+          >
+            Generate answer
+          </button>
+        </form>
+        <div className="w-full md:w-2/3 m-auto text-center rounded bg-gray-50 my-1">
+          <ReactMarkdown className="p-3">{answer}</ReactMarkdown>
+        </div>
+      </div>
     </>
   );
 }
